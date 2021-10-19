61ed70cae0339515219812a4933d03f65fec48f6
Randy Fay <randy@randyfay.com>
2021-10-19 13:45:28 -0600

    Working on 8.0 install

--- 8.0/build.sh
+++ 8.0/build.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 set -eu -o pipefail
 
-BASE=${BASE:-drud/mysql:$(cat base_version.txt)}
-MAJOR=${BASE%.*}
-
-cp -r ../xtrabackup-8.0/install files
+PACKAGE_VERSION=$(cat mysql_version.txt)
+MYSQL_MINOR=${PACKAGE_VERSION%%-*}
+MYSQL_MAJOR=${MYSQL_MINOR%.*}
+XTRABACKUP_VERSION=$(cat xtrabackup_version.txt)
 
 set -x
 # Build only current architecture and load into docker
-docker buildx build --target=mysql -t ${BASE} -t ${MAJOR} --load .
+docker buildx build --build-arg=XTRABACKUP_VERSION="${XTRABACKUP_VERSION}" --build-arg="PACKAGE_VERSION=${PACKAGE_VERSION}" --target=mysql -t ${MYSQL_MAJOR} -t ${MYSQL_MINOR} --load .
