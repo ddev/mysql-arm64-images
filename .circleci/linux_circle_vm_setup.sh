ab465ccbcfcd3787d76090ecb47f839cd181c354
Randy Fay <randy@randyfay.com>
2021-10-18 19:53:25 -0600

    Use buildx of right arch

--- .circleci/linux_circle_vm_setup.sh
+++ .circleci/linux_circle_vm_setup.sh
@@ -12,7 +12,7 @@ sudo apt-get -qq update && sudo apt-get -qq install -y docker-ce-cli binfmt-supp
 
 
 # Get recent buildx
-mkdir -p ~/.docker/cli-plugins && curl -sSL -o ~/.docker/cli-plugins/docker-buildx https://github.com/docker/buildx/releases/download/v0.6.3/buildx-v0.6.3.linux-amd64 && chmod +x ~/.docker/cli-plugins/docker-buildx
+mkdir -p ~/.docker/cli-plugins && curl -sSL -o ~/.docker/cli-plugins/docker-buildx https://github.com/docker/buildx/releases/download/v0.6.3/buildx-v0.6.3.linux-arm64 && chmod +x ~/.docker/cli-plugins/docker-buildx
 
 docker buildx version
 
