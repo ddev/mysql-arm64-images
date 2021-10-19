dfedce0465229e4ec1ea366772800a826c71a759
Randy Fay <randy@randyfay.com>
2021-10-19 15:28:36 -0600

    XTRABACKUP_PACKAGE_VERSION

--- 8.0/push.sh
+++ 8.0/push.sh
@@ -5,8 +5,8 @@ REPO_NAME=drud/mysql
 MYSQL_PACKAGE_VERSION=$(cat mysql_version.txt)
 MYSQL_MINOR=${MYSQL_PACKAGE_VERSION%%-*}
 MYSQL_MAJOR=${MYSQL_MINOR%.*}
-XTRABACKUP_VERSION=$(cat xtrabackup_version.txt)
+XTRABACKUP_PACKAGE_VERSION=$(cat xtrabackup_version.txt)
 
 set -x
 # Build only current architecture and load into docker
-docker buildx build --build-arg=XTRABACKUP_VERSION="${XTRABACKUP_VERSION}" --build-arg="MYSQL_PACKAGE_VERSION=${MYSQL_PACKAGE_VERSION}" --target=mysql -t ${REPO_NAME}:${MYSQL_MINOR} -t ${REPO_NAME}:${MYSQL_MAJOR}  --push --platform=linux/arm64,linux/amd64 .
+docker buildx build --build-arg=XTRABACKUP_PACKAGE_VERSION="${XTRABACKUP_VERSION}" --build-arg="MYSQL_PACKAGE_VERSION=${MYSQL_PACKAGE_VERSION}" --target=mysql -t ${REPO_NAME}:${MYSQL_MINOR} -t ${REPO_NAME}:${MYSQL_MAJOR}  --push --platform=linux/arm64,linux/amd64 .
