9e6793d33a1a848492a473a280da610a2dcd887f
Randy Fay <randy@randyfay.com>
2021-10-19 09:06:43 -0600

    Need lsb-release to get percona repo

--- 5.7/push.sh
+++ 5.7/push.sh
@@ -4,6 +4,7 @@ set -eu -o pipefail
 MINORVERSION=${BASE:-drud/mysql:$(cat base_version.txt)}
 MAJORVERSION=${MINORVERSION%.*}
 
+echo "Pushing mysql:$MINORVERSION"
 set -x
 # Build only current architecture and load into docker
 docker buildx build --target=mysql -t ${MINORVERSION} -t ${MAJORVERSION}  --push --platform=linux/arm64,linux/amd64 .
