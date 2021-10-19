166bdcbf276a85d0f1a13e534851fdccd65270ad
Randy Fay <randy@randyfay.com>
2021-10-19 07:57:32 -0600

    Try building on amd64

--- .circleci/linux_circle_vm_setup.sh
+++ .circleci/linux_circle_vm_setup.sh
@@ -3,6 +3,19 @@
 set -eu -o pipefail
 set -x
 
+case $(arch) in
+  x86_64)
+    ARCH=amd64
+    ;;
+  aarch64)
+    ARCH=arm64
+    ;;
+  *)
+    echo "Unknown architecture" && exit 1
+    ;;
+esac
+
+
 # Get recent qemu to avoid constant qemu crashes on Ubuntu 20.04
 # Incomprehensible discussions of the problem at
 # https://bugs.launchpad.net/ubuntu/+source/qemu/+bug/1928075
@@ -12,7 +25,7 @@ sudo apt-get -qq update && sudo apt-get -qq install -y docker-ce-cli binfmt-supp
 
 
 # Get recent buildx
-mkdir -p ~/.docker/cli-plugins && curl -sSL -o ~/.docker/cli-plugins/docker-buildx https://github.com/docker/buildx/releases/download/v0.6.3/buildx-v0.6.3.linux-arm64 && chmod +x ~/.docker/cli-plugins/docker-buildx
+mkdir -p ~/.docker/cli-plugins && curl -sSL -o ~/.docker/cli-plugins/docker-buildx https://github.com/docker/buildx/releases/download/v0.6.3/buildx-v0.6.3.linux-${ARCH} && chmod +x ~/.docker/cli-plugins/docker-buildx
 
 docker buildx version
 
@@ -20,4 +33,4 @@ if ! docker buildx inspect ddev-builder-multi --bootstrap >/dev/null; then docke
 docker buildx inspect --bootstrap
 
 # Install github's gh tool
-wget -O /tmp/gh.deb https://github.com/cli/cli/releases/download/v2.1.0/gh_2.1.0_linux_arm64.deb && sudo dpkg -i /tmp/gh.deb >/dev/null
+wget -O /tmp/gh.deb https://github.com/cli/cli/releases/download/v2.1.0/gh_2.1.0_linux_${ARCH}.deb && sudo dpkg -i /tmp/gh.deb >/dev/null
