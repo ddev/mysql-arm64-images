85a5bcdd730f29503f3c09a65ec5693515442584
Randy Fay <randy@randyfay.com>
2021-10-19 22:06:00 +0000

    Fix broken var

--- 8.0/push.sh
+++ 8.0/push.sh
@@ -9,4 +9,4 @@ XTRABACKUP_PACKAGE_VERSION=$(cat xtrabackup_version.txt)
 
 set -x
 # Build only current architecture and load into docker
-docker buildx build --build-arg=XTRABACKUP_PACKAGE_VERSION="${XTRABACKUP_VERSION}" --build-arg="MYSQL_PACKAGE_VERSION=${MYSQL_PACKAGE_VERSION}" --target=mysql -t ${REPO_NAME}:${MYSQL_MINOR} -t ${REPO_NAME}:${MYSQL_MAJOR}  --push --platform=linux/arm64,linux/amd64 .
+docker buildx build --build-arg=XTRABACKUP_PACKAGE_VERSION="${XTRABACKUP_PACKAGE_VERSION}" --build-arg="MYSQL_PACKAGE_VERSION=${MYSQL_PACKAGE_VERSION}" --target=mysql -t ${REPO_NAME}:${MYSQL_MINOR} -t ${REPO_NAME}:${MYSQL_MAJOR}  --push --platform=linux/arm64,linux/amd64 .
