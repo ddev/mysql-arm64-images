6cea4a2e25aa66dee6c099e559ff3379b7da62cb
Randy Fay <randy@randyfay.com>
2021-10-19 13:53:22 -0600

    package version args

--- 8.0/build.sh
+++ 8.0/build.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
 set -eu -o pipefail
 
-PACKAGE_VERSION=$(cat mysql_version.txt)
-MYSQL_MINOR=${PACKAGE_VERSION%%-*}
+REPO_NAME=drud/mysql
+MYSQL_PACKAGE_VERSION=$(cat mysql_version.txt)
+MYSQL_MINOR=${MYSQL_PACKAGE_VERSION%%-*}
 MYSQL_MAJOR=${MYSQL_MINOR%.*}
 XTRABACKUP_VERSION=$(cat xtrabackup_version.txt)
 
 set -x
 # Build only current architecture and load into docker
-docker buildx build --build-arg=XTRABACKUP_VERSION="${XTRABACKUP_VERSION}" --build-arg="PACKAGE_VERSION=${PACKAGE_VERSION}" --target=mysql -t ${MYSQL_MAJOR} -t ${MYSQL_MINOR} --load .
+docker buildx build --build-arg=XTRABACKUP_VERSION="${XTRABACKUP_VERSION}" --build-arg="MYSQL_PACKAGE_VERSION=${MYSQL_PACKAGE_VERSION}" --target=mysql -t ${REPO_NAME}:${MYSQL_MAJOR} -t ${REPO_NAME}:${MYSQL_MINOR} --load .
