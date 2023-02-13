b68a1b7dd94da78d8cdd10594b6559458e51c680
Randy Fay <randy@randyfay.com>
2023-02-12 18:28:16 -0700

    Upgrade mysql to 8.0.31  (#2)

--- 8.0/build.sh
+++ 8.0/build.sh
@@ -9,4 +9,4 @@ XTRABACKUP_PACKAGE_VERSION=$(cat xtrabackup_version.txt)
 
 set -x
 # Build only current architecture and load into docker
-docker buildx build --build-arg=XTRABACKUP_PACKAGE_VERSION="${XTRABACKUP_PACKAGE_VERSION}" --build-arg="MYSQL_PACKAGE_VERSION=${MYSQL_PACKAGE_VERSION}" --target=mysql -t ${REPO_NAME}:${MYSQL_MAJOR} -t ${REPO_NAME}:${MYSQL_MINOR} .
+docker buildx build --progress plain --no-cache --build-arg=XTRABACKUP_PACKAGE_VERSION="${XTRABACKUP_PACKAGE_VERSION}" --build-arg="MYSQL_PACKAGE_VERSION=${MYSQL_PACKAGE_VERSION}" --target=mysql -t ${REPO_NAME}:${MYSQL_MAJOR} -t ${REPO_NAME}:${MYSQL_MINOR} .
