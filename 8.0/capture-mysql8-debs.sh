80905f5d815587f4239c8a97b4a30764f04c1a29
Randy Fay <randy@randyfay.com>
2023-07-14 15:07:00 -0600

    Update to 8.0.33 and 5.7.42 (#6)

---
+++ 8.0/capture-mysql8-debs.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+set -eu -o pipefail
+set -x
+
+V=mysql-8.0.33
+
+rm -rf ${V} && mkdir -p $V/archives-{amd64,arm64}/partial && chmod -R ugo+rw $V/archives-{amd64,arm64} && sudo chown -R root $V/archives-{amd64,arm64}
+
+for PLATFORM in arm64 amd64; do
+  docker run --rm --platform linux/$PLATFORM -v $PWD/$V/archives-$PLATFORM:/var/cache/apt/archives --name mysql-$PLATFORM --rm ubuntu:20.04 bash -c "apt-get update && apt-get upgrade -y && apt-get install -y --download-only mysql-server"
+done
+
+rm -rf ${V}/archives*/{lock,partial}
